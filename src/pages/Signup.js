a1c452e89d144a3b43cafaa6bead286b07bd9bcc
Chris5477 <petitcolaschristopher@gmail.com>
2022-04-05 14:09:42 +0200

    add link to home page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+import back from "../assets/icon/back.svg"
 import FormUser from "../components/Home/FormUser";
 
 const Signup = () => {
 	return (
 		<div className="signup">
 			<FormUser id={"pseudo-sign"} id2={"password-sign"} classCSS={"signup-form"} />
+			<Link to="/"><img src={back} alt="back arrow" /></Link>
 		</div>
 	);
 };
